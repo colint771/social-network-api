c8f278118797fd070d9878346993f1715b760b4f
colint771 <colint771@gmail.comgit config --global user.email colint771@gmail.com>
2023-03-07 20:48:53 -0500

    debug

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    process.env.MONGODB_URI || "", // need link to db
+    process.env.MONGODB_URI || "mongodb+srv://colint771:Hartwell230pc!@social-network-api.gcd7c6m.mongodb.net/test", // need link to db
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
