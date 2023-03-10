f70f1ce642fa8cd6620cec9506ce7512f12a1e01
colint771 <colint771@gmail.comgit config --global user.email colint771@gmail.com>
2023-03-09 19:20:52 -0500

    db connection fix

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+const dotenv= require("dotenv");
 
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb+srv://colint771:Hartwell230pc!@social-network-api.gcd7c6m.mongodb.net/test", // need link to db
+    process.env.MONGODB_URI || "mongodb+srv://colint771:%3Cpassword%3E@social-network-api.gcd7c6m.mongodb.net/test", // need link to db
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
